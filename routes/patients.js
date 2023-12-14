6b98759fade5ba2437ed9bf5640c8b22f5aaf37c
Michael <mystermitch18@gmail.com>
2023-12-13 21:22:18 -0500

    final

--- routes/patients.js
+++ routes/patients.js
@@ -16,7 +16,19 @@ router.get("/", async (req,res) =>{
 })
 
 // Add a new patient
-router.post('/', newPatientRegistration)
+router.post("/", async (req, res) => {
+    const patient = new Patient({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName
+    })
+
+    try {
+        const savedPatient = await patient.save();
+        res.json(savedPatient)
+    } catch (err) {
+        res.json({message : err})
+    }
+})
 
 // Find patient by ID
 router.get('/:patientId', async (req, res) =>{
